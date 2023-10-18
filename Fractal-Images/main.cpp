f630584306e55cf5a3178d00af51968201d3eb85
Miguel Yepes <miguelyegarcia94@gmail.com>
2023-10-18 02:58:11 -0600

    Improving the image

--- Fractal-Images/main.cpp
+++ Fractal-Images/main.cpp
@@ -23,18 +23,20 @@ int main()
         for (int x = 0; x < WIDTH; x++)
         {
             //Scaling making the range from -1 to 1;
-            double xFractal = (x - WIDTH / 2) * 2.0 / WIDTH; //first we take the half of the width so we get simetrical with 0 (-400 to 400) then we multiply this with the fraction 2/width to get 1 (-1 to 1)
+            double xFractal = (x - WIDTH / 2 + 200) * 2.0 / HEIGHT; //first we take the half of the width so we get simetrical with 0 (-400 to 400) then we multiply this with the fraction 2/width to get 1 (-1 to 1)
             double yFractal = (y - HEIGHT / 2) * 2.0 / HEIGHT;
 
             int iterations = Mandelbrot::getIteration(xFractal, yFractal);
 
-            uint8_t red = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
+            uint8_t color = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
 
-            bitmap.setPixel(x, y, red, red, red);
+            color = color * color * color;
+
+            bitmap.setPixel(x, y, 0, color, 0);
 
             //cheking if range is proper
-            if (red < min) min = red;
-            if (red > max) max = red;
+            if (color < min) min = color;
+            if (color > max) max = color;
 
             //bitmap.setPixel(x, y, 255, 0, 0);
         }
