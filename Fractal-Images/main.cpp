25142c768f6da2f59fbae9db198e0bbb1d0f78f5
Miguel Yepes <miguelyegarcia94@gmail.com>
2023-10-18 21:33:34 -0600

    Used histogram

--- Fractal-Images/main.cpp
+++ Fractal-Images/main.cpp
@@ -14,9 +14,6 @@ int main()
     int const WIDTH = 800;
     int const HEIGHT = 600;
 
-    double min = 999999;
-    double max = -999999;
-
     unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS]{0}); //storing how many pixels for each iteration
     unique_ptr<int[]> fractal(new int[WIDTH*HEIGHT]{0});   //storing the iterations of each pixel
 
@@ -38,22 +35,39 @@ int main()
             {
                 histogram[iterations]++;
             }
-            
-
-            uint8_t color = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
-
-            color = color * color * color;
-
-            bitmap.setPixel(x, y, 0, color, 0);
-
-            //cheking if range is proper
-            if (color < min) min = color;
-            if (color > max) max = color;
-
-            //bitmap.setPixel(x, y, 255, 0, 0);
         }
     }
 
+    int total = 0;
+    for (int i = 0; i < Mandelbrot::MAX_ITERATIONS; i++)
+    {
+        total += histogram[i];
+    }
+
+
+    for (int y = 0; y < HEIGHT; y++)
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+
+            int iterations = fractal[y * WIDTH + x];
+
+            uint8_t color = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
+            
+            double hue = 0;
+
+            for (int i = 0; i <= iterations; i++)
+            {
+                hue += (double)histogram[i]/total;
+            }
+
+            uint8_t red = 0;
+            uint8_t green = hue * 255;
+            uint8_t blue = 0;
+
+            bitmap.setPixel(x, y, red, green, blue);
+        }
+    }
 
     bitmap.write("test.bmp");
 
